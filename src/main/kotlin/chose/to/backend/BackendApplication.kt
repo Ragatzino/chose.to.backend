2af46de58615f4255a3000c9deb18454b10cfa45
User <antoinebrunetti@gmail.com>
2019-09-24 21:23:20 +0200

    init

---
+++ src/main/kotlin/chose/to/backend/BackendApplication.kt
@@ -0,0 +1,13 @@
+package chose.to.backend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<BackendApplication>(*args)
+	println("lol")
+
+}
